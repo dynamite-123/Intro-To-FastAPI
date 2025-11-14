7b8d9f8890b4813fec8904fa9296d9d372eabfee
dynamite-123 <anirudhkashyap182005@gmail.com>
2025-11-14 22:26:20 +0530

    removed response models

--- app/main.py
+++ app/main.py
@@ -35,35 +35,6 @@ def get_book_by_id(book_id: int):
             return book
     raise HTTPException(status_code=404, detail="Book not found")
 
-
-@app.post("/books/create", response_model=BookOut)
-def create_new_book(book: Book):
-    new_book = book.model_dump()
-    # Generate new ID
-    new_id = max([b["id"] for b in books], default=-1) + 1
-    new_book["id"] = new_id
-    books.append(new_book)
-    return new_book
-
-@app.delete("/books/delete/{book_id}")
-def delete_book(book_id: int):
-    N = len(books)
-    for i in range(N):
-        if books[i]["id"] == book_id:
-            deleted = books.pop(i)
-            return {"message": "Book deleted successfully"}
-    raise HTTPException(status_code=404, detail="Book not found")
-
-@app.put("/books/update/{book_id}", response_model=BookOut)
-def update_book(book_id: int, book: Book):
-    N = len(books)
-    for i in range(N):
-        if books[i]["id"] == book_id:
-            books[i] = book.model_dump()
-            books[i]["id"] = book_id
-            return books[i]
-    raise HTTPException(status_code=404, detail="Book not found")
-
 @app.get("/borrowed", response_model=List[BookOut])
 def get_borrowed_books():
     borrowed_books = []
@@ -73,3 +44,32 @@ def get_borrowed_books():
     return borrowed_books
 
 
+@app.post("/books/create")
+def create_new_book(book: Book):
+    new_book = book.model_dump()
+    # Generate new ID
+    new_id = max([b["id"] for b in books], default=-1) + 1
+    new_book["id"] = new_id
+    books.append(new_book)
+    return {"message": "Book created successfully", "book": new_book}
+
+
+@app.delete("/books/delete/{book_id}")
+def delete_book(book_id: int):
+    N = len(books)
+    for i in range(N):
+        if books[i]["id"] == book_id:
+            deleted = books.pop(i)
+            return {"message": "Book deleted successfully", "book": deleted}
+    raise HTTPException(status_code=404, detail="Book not found")
+
+
+@app.put("/books/update/{book_id}")
+def update_book(book_id: int, book: Book):
+    N = len(books)
+    for i in range(N):
+        if books[i]["id"] == book_id:
+            books[i] = book.model_dump()
+            books[i]["id"] = book_id
+            return {"message": "Book updated successfully", "book": books[i]}
+    raise HTTPException(status_code=404, detail="Book not found")
